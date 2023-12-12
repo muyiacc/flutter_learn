9296f0df5343dc4aab2385f1002f268eb467eda2
muyiacc <115904774+muyiacc@users.noreply.github.com>
2023-12-12 23:55:01 +0800

    backup mian.dart to main16_namedroute.dart

---
+++ lib/main16_namedroute.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './pages/namedroute/circle_of_friends.dart';
+import './pages/namedroute/collect.dart';
+import './pages/namedroute/mailbox.dart';
+import './pages/namedroute/nearby.dart';
+import './pages/namedroute/setting.dart';
+import './pages/namedroute/home1.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+
+      // 路由配置
+      // 初始化路由
+      initialRoute: "/",
+      // 添加 routes
+      routes: {
+        "/" : (context) => HomePage(),
+        "/circleoffriends" : (context) => CircleOfFriends(),
+        "/collect" : (context) => Collect(),
+        "/mailbox" : (context) => Mailbox(),
+        "/nearby" : (context) => Nearby(),
+        "/setting" : (context) => Setting(),
+      },
+    );
+  }
+}
