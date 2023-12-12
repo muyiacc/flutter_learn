629579875d03333691dec236dcb2ce74a8e7a69a
muyiacc <115904774+muyiacc@users.noreply.github.com>
2023-12-12 12:07:21 +0800

    Tabbar combined with previous bottom navigation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -4,14 +4,66 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  // 生命周期函数：当组件初始化的时候就会执行
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // 初始化：
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("主页"),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          // leading: const Icon(Icons.home), // 左侧的按钮图标
+          // title: const Text("Flutter App"),
+          // backgroundColor: Colors.white,
+          // actions: [
+          //   // 右侧的按钮图标
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.more_horiz)),
+          // ],
+
+          title: TabBar(controller: _tabController, tabs: const [
+            Tab(child: Text("关注")),
+            Tab(child: Text("推荐")),
+            Tab(child: Text("同城")),
+          ]),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView(
+              children: const [
+                ListTile(
+                  title: Text("关注"),
+                ),
+              ],
+            ),
+            ListView(
+              children: const [
+                ListTile(
+                  title: Text("推荐"),
+                ),
+              ],
+            ),
+            ListView(
+              children: const [
+                ListTile(
+                  title: Text("同城"),
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 }
